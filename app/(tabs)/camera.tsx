43179ab6c32984610471c6db2bf27548563afb7a
TuanMaiz <tuanmai1509work@gmail.com>
2024-10-14 00:13:05 +0700

    add tensorcamera

--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -4,11 +4,14 @@ import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
 import { useNavigation } from 'expo-router';
 import React, { useEffect, useRef } from 'react';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button, Dimensions, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import * as tf from '@tensorflow/tfjs'
 import { cameraWithTensors } from '@tensorflow/tfjs-react-native'
+import * as mobilenet from '@tensorflow-models/mobilenet'
 import Canvas, { CanvasRenderingContext2D } from 'react-native-canvas';
 
+const TensorCamera = cameraWithTensors(CameraView);
+const { width, height } = Dimensions.get('window');
 
 export default function App() {
   const [facing, setFacing] = useState<CameraType>('back');
@@ -17,13 +20,26 @@ export default function App() {
   const [isTfReady, setIsTfReady] = useState<boolean>(false);
   const [on, setOn] = useState<boolean>(false);
 
+  const [model, setModel] = useState<mobilenet.MobileNet>();
+  const canvas = useRef<Canvas>();
+  let context = useRef<CanvasRenderingContext2D>();
+
+  let textureDims;
+  Platform.OS === 'ios'
+    ? (textureDims = { height: 1920, width: 1080 })
+    : (textureDims = { height: 1200, width: 1600 });
   useEffect(() => {
     async function setup()
     {
       await tf.ready();
+      const model = await mobilenet.load();
+      setModel(model);
       setIsTfReady(true);
     }
     setup();
+    return () => {
+      setOn(false)
+    }
   }, [])
 
   if (!permission) {
@@ -44,44 +60,109 @@ export default function App() {
   function toggleCameraFacing() {
     setFacing(current => (current === 'back' ? 'front' : 'back'));
   }
-  const handleCameraStream = (images: IterableIterator<tf.Tensor3D>) => {
+  const handleCanvas = async (can: Canvas) => {
+    if (can) {
+      can.width = width;
+      can.height = height;
+      const ctx: CanvasRenderingContext2D = can.getContext('2d');
+      context.current = ctx;
+      ctx.strokeStyle = 'red';
+      ctx.fillStyle = 'red';
+      ctx.lineWidth = 3;
+      canvas.current = can;
+    }
+  };
+  function drawRectangle(
+    predictions: any,
+    nextImageTensor: any
+  ) {
+    if (!context.current || !canvas.current) {
+      console.log('no context or canvas');
+      return;
+    }
+
+    console.log(predictions);
+
+    // to match the size of the camera preview
+    const scaleWidth = width / nextImageTensor.shape[1];
+    const scaleHeight = height / nextImageTensor.shape[0];
+
+    const flipHorizontal = Platform.OS === 'ios' ? false : true;
+
+    // We will clear the previous prediction
+    context.current.clearRect(0, 0, width, height);
+
+    // Draw the rectangle for each prediction
+    for (const prediction of predictions) {
+      const [x, y, width, height] = prediction.bbox;
+
+      // Scale the coordinates based on the ratios calculated
+      const boundingBoxX = flipHorizontal
+        ? canvas.current.width - x * scaleWidth - width * scaleWidth
+        : x * scaleWidth;
+      const boundingBoxY = y * scaleHeight;
+
+      // Draw the bounding box.
+      context.current.strokeRect(
+        boundingBoxX,
+        boundingBoxY,
+        width * scaleWidth,
+        height * scaleHeight
+      );
+      // Draw the label
+      context.current.fillText(
+        prediction.class,
+        boundingBoxX - 5,
+        boundingBoxY - 5
+      );
+    }
+  }
+  function handleCameraStream(images: any) {
+    console.log('handleCameraStream');
     const loop = async () => {
-      if (canvasRef.current) {
-        const nextImageTensor = images.next().value;
-        if (nextImageTensor) {
-          // Perform AI detection here
-          // For example purposes, we'll just draw a rectangle
-          const ctx = canvasRef.current.getContext('2d');
-          if (ctx) {
-            ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-            ctx.strokeStyle = 'red';
-            ctx.lineWidth = 2;
-            ctx.strokeRect(50, 50, 100, 100);
-          }
-          tf.dispose(nextImageTensor);
-        }
-      }
+      const nextImageTensor = images.next().value;
+      console.log('looping')
+      // if (!model || !nextImageTensor) throw new Error('no model');
+
+      // model
+      //   .classify(nextImageTensor)
+      //   .then((predictions) => {
+      //     drawRectangle(predictions, nextImageTensor);
+      //   })
+      //   .catch((err) => {
+      //     console.log(err);
+      //   });
+
       requestAnimationFrame(loop);
     };
     loop();
-  };
+  }
 
   return (
     <View style={styles.container}>
       {
         on ? (
           <View style={styles.container}>
-            <CameraView style={styles.camera} facing={facing}>
-              <TouchableOpacity style={styles.backButton} onPress={() => setOn(false)}>
+            <TensorCamera
+            // Standard Camera props
+            style={styles.camera}
+            facing={facing}
+            // Tensor related props
+            cameraTextureHeight={textureDims.height}
+            cameraTextureWidth={textureDims.width}
+            resizeHeight={200}
+            resizeWidth={152}
+            resizeDepth={3}
+            onReady={handleCameraStream}
+            autorender={true}
+            useCustomShadersToResize={false}
+          />
+            <Canvas style={styles.canvas} ref={handleCanvas} />
+            <TouchableOpacity style={styles.backButton} onPress={() => setOn(false)}>
                 <Ionicons name="arrow-back" size={24} color="white" />
               </TouchableOpacity>
-              <View style={styles.buttonContainer}>
-                <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-                  <Text style={styles.text}>Xoay Camera</Text>
-                </TouchableOpacity>
-              </View>
-            </CameraView>
           </View>
+          
         ) : (
           <ThemedView style={styles.buttonContainer}>
             <TouchableOpacity style={styles.buttonTurnOnCamera} onPress={() => setOn(true)}>
@@ -120,6 +201,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
     borderRadius: 999,
     backgroundColor: 'white',
+    padding: 10
   },
   buttonTurnOnCamera: {
     flex: 1,
@@ -139,6 +221,12 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 60,
     left: 30,
-    zIndex: 10,
+    zIndex: 1000,
+  },
+  canvas: {
+    position: 'absolute',
+    zIndex: 100,
+    width: '100%',
+    height: '100%',
   },
 });
